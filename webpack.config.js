3300ac7642ca08d8b4e79c67bd25b14e714d795f
Bonggt <kepy1106@gmail.com>
2019-03-08 17:54:04 +0900

    Output Management

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  entry: './src/index.js',
+  entry: {
+    app: "./src/index.js",
+    print: "./src/print.js",
+  },
+  plugins: [
+    // new CleanWebpackPlugin(["dist/*"]) for < v2 versions of CleanWebpackPlugin
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Output Management"
+    })
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"],
-      }
-    ]
-  }
 };
