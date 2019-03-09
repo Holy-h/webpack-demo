2f248c4b1b057c84e27cadab36fd55299bdff563
Bonggt <kepy1106@gmail.com>
2019-03-09 18:01:50 +0900

    webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ module.exports = {
     print: "./src/print.js",
   },
   devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist"
+  },
   plugins: [
     // new CleanWebpackPlugin(["dist/*"]) for < v2 versions of CleanWebpackPlugin
     new CleanWebpackPlugin(),
